4d7213d93d0f77c14ce73ba84d872c84f8993acc
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-09-25 14:39:24 +0200

    fixed ForgotPasswordMail

---
+++ src/pages/ForgotPasswordMail.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+
+function ForgotPasswordMail() {
+  return (
+    <>
+      <Container>
+        <Row className="d-flex justify-content-center align-items-center  vh-100">
+          <Col xs={12} md={8} lg={6} xl={5}>
+            <div className="p-5 custom-box-shadow bg-body-tertiary rounded-3">
+              <h1 className="text-center ">Glömt lösenord</h1>
+              <p className="text-center mb-5">
+                Skriv in din mejl adress, så får du en länk på din mejl där du kan byta lösenord
+              </p>
+              <Form>
+                <Form.Group controlId="formEmail">
+                  <Form.Label>Fyll i din Mejl</Form.Label>
+                  <Form.Control className="p-2" type="email" placeholder="Ange din e-postadress" />
+                </Form.Group>
+                <div className="d-flex justify-content-between mt-4">
+                  <Button variant="secondary">Tillbaka</Button>
+                  <Button variant="primary">Skicka</Button>
+                </div>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default ForgotPasswordMail
--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -4,11 +4,13 @@ import StartPage from '../pages/StartPage'
 import IndividualMovie from '../pages/IndividualMovie'
 import React from 'react'
 import ConfirmationPage from '../pages/ConfirmationPage'
+import ForgotPasswordMail from '../pages/ForgotPasswordMail'
 
 // paths to use with the router and with the main menu
 export default [
   { path: '/', element: <StartPage />, menuLabel: 'Start' },
   { path: '/ConfirmationPage', element: <ConfirmationPage />, menuLabel: 'ConfirmationPage' },
+  { path: '/ForgotPasswordMail', element: <ForgotPasswordMail />, menuLabel: 'ForgotPasswordMail' },
   { path: '/Member', element: <MemberPage />, menuLabel: '' },
   { path: '/individual', element: <IndividualMovie /> },
 ]
