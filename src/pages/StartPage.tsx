d02b6ba82793247af9f0f086366de77c079e5801
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-09-25 10:14:58 +0200

    Small changes

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import Card from 'react-bootstrap/Card'
-import Nav from 'react-bootstrap/Nav'
+import { Container, Row, Col, Card, Nav } from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar'
 import Carousel from 'react-bootstrap/Carousel'
 import Image from 'react-bootstrap/Image'
@@ -11,14 +9,114 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import { BsArrowDown, BsArrowDownCircle, BsClock, BsCalendar } from 'react-icons/bs'
 import Badge from 'react-bootstrap/Badge'
 
-const booking = {
-    title: 'Avengers',
-    timeHour: 2,
-    timeMin: 15,
-    type: 'Action',
-    age: 15,
-    dates: '24/09',
-    sceenTime: '19:30',
+const movies = {
+    avengers: {
+        title: 'Avengers',
+        poster: {
+            liggande: 'https://pbs.twimg.com/media/D4pgj_BVUAAbCTd.jpg:large',
+            stående: 'https://img.fruugo.com/product/7/41/14532417_max.jpg',
+        },
+        timeHour: 2,
+        timeMin: 15,
+        type: 'Action',
+        age: 15,
+        dates: '24/09',
+        sceenTime: '19:30',
+    },
+    interstellar: {
+        title: 'Interstellar',
+        poster: {
+            liggande:
+                'https://www.wallpaperflare.com/static/912/656/60/interstellar-movie-movies-monochrome-movie-poster-wallpaper.jpg',
+            stående: 'https://static.posters.cz/image/750/poster/interstellar-one-sheet-i23157.jpg',
+        },
+        timeHour: 2,
+        timeMin: 20,
+        type: 'Action',
+        age: 15,
+        dates: '24/09',
+        sceenTime: '19:30',
+    },
+    shangchi: {
+        title: 'Shang-Chi',
+        poster: {
+            liggande: 'https://wallpapers.com/images/hd/shang-chi-official-movie-poster-kgiycfo4zeh71c5c.jpg',
+            stående:
+                'https://static.posters.cz/image/1300/poster/shang-chi-and-the-legend-of-the-ten-rings-flex-i114390.jpg',
+        },
+        timeHour: 2,
+        timeMin: 20,
+        type: 'Action',
+        age: 15,
+        dates: '24/09',
+        sceenTime: '19:30',
+    },
+    StarWars: {
+        title: 'Star Wars',
+        poster: {
+            liggande: 'https://wallpapers.com/images/hd/shang-chi-official-movie-poster-kgiycfo4zeh71c5c.jpg',
+            stående: 'https://images-cdn.ubuy.co.in/63ef0a397f1d781bea0a2464-star-wars-rogue-one-movie-poster.jpg',
+        },
+        timeHour: 2,
+        timeMin: 50,
+        type: 'Action',
+        age: 15,
+        dates: '24/09',
+        sceenTime: '19:30',
+    },
+
+    StarWars2: {
+        title: 'Star Wars',
+        poster: {
+            liggande: 'https://wallpapers.com/images/hd/shang-chi-official-movie-poster-kgiycfo4zeh71c5c.jpg',
+            stående: 'https://images-cdn.ubuy.co.in/63ef0a397f1d781bea0a2464-star-wars-rogue-one-movie-poster.jpg',
+        },
+        timeHour: 2,
+        timeMin: 50,
+        type: 'Action',
+        age: 15,
+        dates: '24/09',
+        sceenTime: '19:30',
+    },
+    StarWars3: {
+        title: 'Star Wars',
+        poster: {
+            liggande: 'https://wallpapers.com/images/hd/shang-chi-official-movie-poster-kgiycfo4zeh71c5c.jpg',
+            stående: 'https://images-cdn.ubuy.co.in/63ef0a397f1d781bea0a2464-star-wars-rogue-one-movie-poster.jpg',
+        },
+        timeHour: 2,
+        timeMin: 50,
+        type: 'Action',
+        age: 15,
+        dates: '24/09',
+        sceenTime: '19:30',
+    },
+    StarWars4: {
+        title: 'Star Wars',
+        poster: {
+            liggande: 'https://wallpapers.com/images/hd/shang-chi-official-movie-poster-kgiycfo4zeh71c5c.jpg',
+            stående: 'https://images-cdn.ubuy.co.in/63ef0a397f1d781bea0a2464-star-wars-rogue-one-movie-poster.jpg',
+        },
+        timeHour: 2,
+        timeMin: 50,
+        type: 'Action',
+        age: 15,
+        dates: '24/09',
+        sceenTime: '19:30',
+    },
+    StarWars5: {
+        title: 'Star Wars',
+        poster: {
+            liggande: 'https://wallpapers.com/images/hd/shang-chi-official-movie-poster-kgiycfo4zeh71c5c.jpg',
+            stående: 'https://images-cdn.ubuy.co.in/63ef0a397f1d781bea0a2464-star-wars-rogue-one-movie-poster.jpg',
+        },
+        timeHour: 2,
+        timeMin: 50,
+        type: 'Action',
+        age: 15,
+        dates: '24/09',
+        sceenTime: '19:30',
+    },
 }
 
 const ages = ['6-12', '12-25', '25-65', 'Alla åldrar']
@@ -49,13 +147,13 @@ export default function StartPage() {
             </Navbar>
             <Carousel>
                 <Carousel.Item interval={5000}>
-                    <Image src="https://wallpapers.com/images/hd/shang-chi-official-movie-poster-kgiycfo4zeh71c5c.jpg" />
+                    <Image src={movies.shangchi.poster.liggande} />
                 </Carousel.Item>
                 <Carousel.Item interval={5000}>
-                    <Image src="https://www.wallpaperflare.com/static/912/656/60/interstellar-movie-movies-monochrome-movie-poster-wallpaper.jpg" />
+                    <Image src={movies.interstellar.poster.liggande} />
                 </Carousel.Item>
                 <Carousel.Item interval={5000}>
-                    <Image src="https://pbs.twimg.com/media/D4pgj_BVUAAbCTd.jpg:large" />
+                    <Image src={movies.avengers.poster.liggande} />
                 </Carousel.Item>
             </Carousel>
 
@@ -86,23 +184,22 @@ export default function StartPage() {
                     </Button>
                 </Stack>
                 <Row xs={2} xl={4} className="g-2 gy-2">
-                    {Array.from({ length: 4 }).map((_, idx) => (
+                    {Object.values(movies).map((movie, idx) => (
                         <Col key={idx}>
                             <Card className="border">
                                 <div className="overflow-hidden rounded-bottom-0 rounded">
-                                    <Card.Img
-                                        variant="top"
-                                        src="https://img.fruugo.com/product/7/41/14532417_max.jpg"
-                                    />
+                                    <Card.Img variant="top" src={movie.poster.stående} />
                                 </div>
                                 <Card.Body>
-                                    <Card.Title>{booking.title}</Card.Title>
-                                    <Card.Text>{booking.type}</Card.Text>
-                                    <Stack>
-                                        <p className="text-muted">
-                                            {booking.timeHour} tim {booking.timeMin} min
-                                        </p>
-                                        <p className="text-muted">Från {booking.age} År</p>
+                                    <Card.Title>{movie.title}</Card.Title>
+                                    <Card.Text>{movie.type}</Card.Text>
+                                    <Stack direction="horizontal" gap={2} className="mx-auto card-badge ">
+                                        <Badge bg="none" className="border">
+                                            {movie.timeHour} tim {movie.timeMin} min
+                                        </Badge>
+                                        <Badge bg="none" className="border">
+                                            Från {movie.age} År
+                                        </Badge>
                                     </Stack>
                                 </Card.Body>
                                 <Card.Link className="stretched-link" href="#"></Card.Link>
@@ -131,7 +228,7 @@ export default function StartPage() {
                                 <Card.Body>
                                     <Card.Title>Tisdag</Card.Title>
                                     <Badge className="py-2 d-inline-flex align-items-center " bg="secondary">
-                                        <BsCalendar className="me-2" /> {booking.dates}
+                                        <BsCalendar className="me-2" /> {movies.avengers.dates}
                                     </Badge>
                                 </Card.Body>
                             </Card>
@@ -145,20 +242,26 @@ export default function StartPage() {
                     <div className="g-3 py-2 rowcard ">
                         {Array.from({ length: 4 }).map((_, idx) => (
                             <div className="horizontal-card" key={idx}>
-                                <Card style={{ width: '15rem' }} className="border">
+                                <Card style={{ width: '13rem' }} className="border card-horizontal__scroll">
                                     <div className="overflow-hidden rounded-bottom-0 rounded">
-                                        <Card.Img
-                                            variant="top"
-                                            src="https://img.fruugo.com/product/7/41/14532417_max.jpg"
-                                        />
+                                        <Card.Img variant="top" src={movies.avengers.poster.stående} />
                                     </div>
                                     <Card.Body className="text-left">
-                                        <Card.Title>{booking.title}</Card.Title>
-                                        <Card.Text>{booking.type}</Card.Text>
-                                        <Badge className="py-2 d-inline-flex align-items-center " bg="secondary">
+                                        <Card.Title>{movies.avengers.title}</Card.Title>
+                                        <Card.Text>{movies.avengers.type}</Card.Text>
+                                        <Badge className="py-1 d-inline-flex align-items-center " bg="secondary">
                                             <BsClock className="me-2" />
-                                            {booking.sceenTime}
+                                            {movies.avengers.sceenTime}
                                         </Badge>
+                                        <Stack direction="horizontal" gap={3} className="mt-2">
+                                            <Badge bg="none" className="border">
+                                                {movies.avengers.timeHour} tim {movies.avengers.timeMin} min
+                                            </Badge>
+
+                                            <Badge bg="none" className="border">
+                                                Från {movies.avengers.age} År
+                                            </Badge>
+                                        </Stack>
                                     </Card.Body>
 
                                     <Button className="mx-2 mb-2" variant="outline-primary">
@@ -176,20 +279,27 @@ export default function StartPage() {
                     <div className="g-3 py-2 rowcard ">
                         {Array.from({ length: 12 }).map((_, idx) => (
                             <div className="colcard" key={idx}>
-                                <Card style={{ width: '15rem' }} className="border ">
+                                <Card className="border card-horizontal__scroll ">
                                     <div className="overflow-hidden rounded-bottom-0 rounded">
-                                        <Card.Img
-                                            variant="top"
-                                            src="https://img.fruugo.com/product/7/41/14532417_max.jpg"
-                                        />
+                                        <Card.Img variant="top" src={movies.avengers.poster.stående} />
                                     </div>
                                     <Card.Body className="text-left">
-                                        <Card.Title>{booking.title}</Card.Title>
-                                        <Card.Text>{booking.type}</Card.Text>
-                                        <Badge className="py-2 d-inline-flex align-items-center " bg="secondary">
+                                        <Card.Title>{movies.avengers.title}</Card.Title>
+                                        <Card.Text>{movies.avengers.type}</Card.Text>
+
+                                        <Badge className="py- d-inline-flex align-items-center " bg="secondary">
                                             <BsClock className="me-2" />
-                                            {booking.sceenTime}
+                                            {movies.avengers.sceenTime}
                                         </Badge>
+                                        <Stack direction="horizontal" gap={3} className="mt-2">
+                                            <Badge bg="none" className="border">
+                                                {movies.avengers.timeHour} tim {movies.avengers.timeMin} min
+                                            </Badge>
+
+                                            <Badge bg="none" className="border">
+                                                Från {movies.avengers.age} År
+                                            </Badge>
+                                        </Stack>
                                     </Card.Body>
                                     <Button className="mx-2 mb-2" variant="outline-primary">
                                         Boka
