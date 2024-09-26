7db90a102cf8eeef9fc19e262accaf99b0fdccbf
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-09-26 15:31:50 +0200

    fixed some styling

--- src/pages/NewPasswordMail.jsx
+++ src/pages/NewPasswordMail.jsx
@@ -8,10 +8,7 @@ function NewPasswordMail() {
         <Row className="d-flex justify-content-center align-items-center flex-column" style={{ minHeight: '100vh' }}>
           <Col xs={12} md={8} lg={6} xl={5}>
             <div className="p-5 custom-box-shadow bg-body-tertiary rounded-3">
-              <h1 className="text-center ">Glömt lösenord</h1>
-              <p className="text-center mb-5">
-                Skriv in din mejl adress, så får du en länk på din mejl där du kan byta lösenord
-              </p>
+              <h1 className="text-center mb-4">Glömt lösenord</h1>
               <Form>
                 <Form.Group controlId="formEmail mb-1">
                   <Form.Label className="mb-1">Nytt lösenord</Form.Label>
@@ -22,8 +19,7 @@ function NewPasswordMail() {
                   <Form.Control className="p-2" type="text" placeholder="Upprepa lösenord" />
                 </Form.Group>
                 <div className="d-flex justify-content-between mt-4">
-                  <Button variant="outline-primary">Logga in</Button>
-                  <Button variant="primary">Uppdatera</Button>
+                  <Button variant="outline-primary">Uppdatera</Button>
                 </div>
               </Form>
             </div>
