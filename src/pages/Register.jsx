5a7fe2c5c4a79862db5807bb94426131d49f4ae7
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-09-26 15:30:53 +0200

    last small  padding changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,8 +9,8 @@ export default function Register() {
     <Container className="vh-100">
       <Row className="d-flex justify-content-center align-items-center vh-100">
         <Col xs={12} md={8} lg={6} xl={5}>
-          <div className="p-4 bg-body-tertiary custom-box-shadow rounded-3">
-            <h2 className="text-center">Bli medlem</h2>
+          <div className="p-md-5 p-4 bg-body-tertiary custom-box-shadow rounded-3">
+            <h2 className="text-center h1">Bli medlem</h2>
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Fyll i din Mejl</Form.Label>
@@ -19,21 +19,21 @@ export default function Register() {
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Lösenord</Form.Label>
-                <Form.Control type="password" placeholder="Ange Lösenord" />
+                <Form.Control className="p-2" type="password" placeholder="Ange Lösenord" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Upprepa Lösenord</Form.Label>
-                <Form.Control type="password" placeholder="Upprepa Lösenord" />
+                <Form.Control className="p-2" type="password" placeholder="Upprepa Lösenord" />
               </Form.Group>
               <Row>
                 <Col>
-                  <Button variant="primary" onClick={notify}>
+                  <Button variant="outline-primary" onClick={notify}>
                     Bli medlem
                   </Button>
                   <ToastContainer />
                 </Col>
                 <Col className="d-flex justify-content-end">
-                  <Button variant="outline-primary">Logga in</Button>
+                  <Button variant="link">Logga in</Button>
                 </Col>
               </Row>
             </Form>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -7,16 +7,16 @@ export default function SignIn() {
       <Container className="vh-100">
         <Row className="d-flex justify-content-center align-items-center vh-100">
           <Col xs={12} md={8} lg={6} xl={5}>
-            <div className="p-4 bg-body-tertiary custom-box-shadow rounded-3">
-              <h2 className="text-center">Logga in</h2>
+            <div className="p-md-5 p-4 bg-body-tertiary custom-box-shadow rounded-3 ">
+              <h2 className="text-center h1">Logga in</h2>
               <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Fyll i din Mejl</Form.Label>
-                  <Form.Control type="email" placeholder="Ange din e-postadress" />
+                  <Form.Control className="p-2" type="email" placeholder="Ange din e-postadress" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Lösenord</Form.Label>
-                  <Form.Control type="password" placeholder="Ange lösenord" />
+                  <Form.Control className="p-2" type="password" placeholder="Ange lösenord" />
                 </Form.Group>
                 <Stack direction="horizontal">
                   <div>
