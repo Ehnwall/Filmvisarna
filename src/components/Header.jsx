f5bdd6251fd7f959252a75f0189b485e5c589ff0
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-10-04 09:20:52 +0200

    fixing route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
               {' '}
               Se filmer
             </Link>
-            <Link to="/#booking" className="nav-link">
+            <Link to="/#book" className="nav-link">
               {' '}
               Boka film
             </Link>
