4f3a2dd2c0508226c3f138785876b0568a0947e0
AwE9800 <143619434+AwE9800@users.noreply.github.com>
2024-10-07 15:51:32 +0200

    Added user and tickettype tables also added example data

--- backend/seed.js
+++ backend/seed.js
@@ -8,5 +8,32 @@ const dir = path.dirname(dbPath)
 if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true })
 }
-
 const db = new database(dbPath)
+
+const createUserTable = `
+    CREATE TABLE IF NOT EXISTS users (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        firstName TEXT NOT NULL,
+        lastName TEXT NOT NULL
+    );
+`
+
+// const userQuery = `INSERT INTO users (email,password,firstName,lastName) VALUES
+// ('eric.classon@example.com', 'hashedPassword1', 'Erik', 'Classon'), ('alvin.samuelsson@example.com', 'hashedPassword2', 'Alvin', 'Samuelsson'),('dennis.ehnwall@example.com', 'hashedPassword3', 'Dennis', 'Ehnwall'),('kalle.pettersson@example.com', 'hashedPassword4', 'Kalle', 'Pettersson'), ('pontus.boman@example.com', 'hashedPassword5', 'Pontus', 'Boman')`
+
+const createTicketTypeTable = `CREATE TABLE IF NOT EXISTS ticketTypes (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ticketType TEXT NOT NULL,
+        price INTEGER NOT NULL
+    );`
+
+// const ticketTypeQuery = `INSERT INTO ticketTypes (ticketType, price) VALUES ('Barn', 80 ), ('Vuxen',  140), ('Pensionär', 120)`
+
+db.exec(createUserTable)
+db.exec(createTicketTypeTable)
+// db.exec(ticketTypeQuery)
+// db.exec(userQuery)
+
+db.close()
