8eb792f229e183e01f9332e09e61b11c70cd114c
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-10-07 13:44:47 +0200

    building the base backend folder

---
+++ backend/seed.js
@@ -0,0 +1,12 @@
+import fs from 'fs'
+import path from 'path'
+import database from 'better-sqlite3'
+
+const dbPath = path.resolve('./backend/db/db.sqlite3')
+
+const dir = path.dirname(dbPath)
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true })
+}
+
+const db = new database(dbPath)
