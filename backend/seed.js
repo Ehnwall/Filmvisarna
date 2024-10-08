eca8dc5669c9a3f19de7d6405e00c971b0a19eaf
AwE9800 <143619434+AwE9800@users.noreply.github.com>
2024-10-08 13:36:05 +0200

    coment out exampledata

--- backend/seed.js
+++ backend/seed.js
@@ -21,14 +21,14 @@ const createUserTable = `
     );
 `
 
-const userQuery = `
-    INSERT INTO users (email, password, firstName, lastName, role) VALUES
-    ('eric.classon@example.com', 'hashedPassword1', 'Erik', 'Classon', 'admin'),
-    ('alvin.samuelsson@example.com', 'hashedPassword2', 'Alvin', 'Samuelsson', 'user'),
-    ('dennis.ehnwall@example.com', 'hashedPassword3', 'Dennis', 'Ehnwall', 'user'),
-    ('kalle.pettersson@example.com', 'hashedPassword4', 'Kalle', 'Pettersson', 'user'),
-    ('pontus.boman@example.com', 'hashedPassword5', 'Pontus', 'Boman', 'user');
-`
+// const userQuery = `
+//     INSERT INTO users (email, password, firstName, lastName, role) VALUES
+//     ('eric.classon@example.com', 'hashedPassword1', 'Erik', 'Classon', 'admin'),
+//     ('alvin.samuelsson@example.com', 'hashedPassword2', 'Alvin', 'Samuelsson', 'user'),
+//     ('dennis.ehnwall@example.com', 'hashedPassword3', 'Dennis', 'Ehnwall', 'user'),
+//     ('kalle.pettersson@example.com', 'hashedPassword4', 'Kalle', 'Pettersson', 'user'),
+//     ('pontus.boman@example.com', 'hashedPassword5', 'Pontus', 'Boman', 'user');
+// `
 
 const createTicketTypeTable = `CREATE TABLE IF NOT EXISTS ticketTypes (
         Id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -36,11 +36,11 @@ const createTicketTypeTable = `CREATE TABLE IF NOT EXISTS ticketTypes (
         price INTEGER NOT NULL
     );`
 
-const ticketTypeQuery = `INSERT INTO ticketTypes (ticketType, price) VALUES ('Barn', 80 ), ('Vuxen',  140), ('Pensionär', 120)`
+// const ticketTypeQuery = `INSERT INTO ticketTypes (ticketType, price) VALUES ('Barn', 80 ), ('Vuxen',  140), ('Pensionär', 120)`
 
 db.exec(createUserTable)
 db.exec(createTicketTypeTable)
-db.exec(ticketTypeQuery)
-db.exec(userQuery)
+// db.exec(ticketTypeQuery)
+// db.exec(userQuery)
 
 db.close()
