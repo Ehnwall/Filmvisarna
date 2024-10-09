afad0b8417b5e2d4326eb326ce82dd83179e6926
BomanStatic <boman.staticsites@gmail.com>
2024-10-09 14:57:18 +0200

    endpoint now gets all the seats by cinemaId

--- backend/src/controller/cinemasController.js
+++ backend/src/controller/cinemasController.js
@@ -1,5 +1,16 @@
+import cinemasService from '../service/cinemasService.js'
+
 const getSeatsByCinemaId = (req, res) => {
-    return res.send({ msg: 'getSeatsByCinemaId' })
+    const { id } = req.params
+    if (isNaN(id)) {
+        return res.status(400).send({ msg: 'Invalid id' })
+    }
+    try {
+        const result = cinemasService.seatsByCinemaId(id)
+        return res.send(result)
+    } catch (e) {
+        return res.status(500).send({ msg: e.message })
+    }
 }
 
 export default { getSeatsByCinemaId }
