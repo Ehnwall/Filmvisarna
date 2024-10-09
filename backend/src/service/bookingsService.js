f864355afa50339f62e5d9a8411eda1f74c3a0d7
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-10-09 14:01:07 +0200

    I have changed so that I can take different id and seats to be able to take information from these endpoints

--- backend/src/service/bookingsService.js
+++ backend/src/service/bookingsService.js
@@ -1,7 +1,24 @@
 import { db } from '../../server.js'
 
 const getBookingFs = (bookingId) => {
-    const getSpecificBooking = 'SELECT * FROM bookings WHERE id = ?'
+    const getSpecificBooking = `
+    SELECT 
+      *
+    FROM 
+      bookings b
+    JOIN 
+      shows s ON b.showId = s.showId
+    JOIN 
+      movies m ON s.movieId = m.movieId
+    JOIN 
+      cinemas c ON s.cinemaId = c.cinemaId
+    JOIN 
+      bookingXtypesXSeats bs ON b.bookingId = bs.bookingId
+    JOIN 
+      ticketTypes tt ON bs.ticketTypeId = tt.ticketTypeId
+    WHERE 
+      b.bookingId = ?
+`
     const statement = db.prepare(getSpecificBooking).get(bookingId)
 
     return statement
