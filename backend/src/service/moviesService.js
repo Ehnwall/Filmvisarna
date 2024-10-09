b1c4c06f8650c312d22f8c30e86ba4b6f2ac1fcf
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-10-09 14:11:51 +0200

    fixing SELECT AS

--- backend/src/service/moviesService.js
+++ backend/src/service/moviesService.js
@@ -11,7 +11,7 @@ const getMovies = () => {
 const getShowsByMovie = (id) => {
     try {
         const showByMovies = db.prepare(
-            'SELECT movies.*, shows.id AS show_id, shows.time  AS show_time, cinemas.name FROM movies INNER JOIN shows ON shows.movieId = movies.id INNER JOIN cinemas ON cinemas.id = shows.cinemaId WHERE movies.Id = ?'
+            'SELECT movies.*, shows.id AS showId, shows.time  AS showTime, cinemas.name AS cinemaName, cinemas.id AS cinemaId FROM movies INNER JOIN shows ON shows.movieId = movies.id INNER JOIN cinemas ON cinemas.id = shows.cinemaId WHERE movies.Id = ?'
         )
         const result = showByMovies.all(id)
         return result
