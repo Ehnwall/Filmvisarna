cedf66db0b2107417309eb9e0510eb39c9e5ab6a
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-10-09 14:47:33 +0200

    post comments

--- backend/src/service/bookingsService.js
+++ backend/src/service/bookingsService.js
@@ -7,18 +7,24 @@ const getBookingFs = (bookingId) => {
     FROM 
       bookings b
     JOIN 
-      shows s ON b.showId = s.showId
+      shows s ON b.showId = s.Id
     JOIN 
-      movies m ON s.movieId = m.movieId
+      movies m ON s.movieId = m.Id
     JOIN 
-      cinemas c ON s.cinemaId = c.cinemaId
+      cinemas c ON s.cinemaId = c.Id
     JOIN 
-      bookingXtypesXSeats bs ON b.bookingId = bs.bookingId
+      bookingXseatsXticket bs ON b.Id = bs.bookingID
+      JOIN cinemaSeats ON bs.cinemaSeatsID = cinemaSeats.Id
     JOIN 
-      ticketTypes tt ON bs.ticketTypeId = tt.ticketTypeId
+      ticketTypes tt ON bs.ticketTypeId = tt.Id
     WHERE 
-      b.bookingId = ?
+      b.Id = ?
 `
+
+    /*
+    I rename, for example, shows as s , tt for ticket tipes and bs for booking seats 
+    */
+
     const statement = db.prepare(getSpecificBooking).get(bookingId)
 
     return statement
