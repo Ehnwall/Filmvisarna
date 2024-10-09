300cb870a2fd0f476c63d52f28d66c54314ed7b5
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-10-09 15:29:42 +0200

    check if id is above all shows length

--- backend/src/service/moviesService.js
+++ backend/src/service/moviesService.js
@@ -7,16 +7,22 @@ const getMovies = () => {
     return stmt
 }
 
-//Get Shows by
 const getShowsByMovie = (id) => {
+    const allShows = db.prepare('SELECT * FROM shows')
+
     try {
         const showByMovies = db.prepare(
             'SELECT movies.*, shows.id AS showId, shows.time  AS showTime, cinemas.name AS cinemaName, cinemas.id AS cinemaId FROM movies INNER JOIN shows ON shows.movieId = movies.id INNER JOIN cinemas ON cinemas.id = shows.cinemaId WHERE movies.Id = ?'
         )
+        if (id > allShows.length) {
+            throw new Error(
+                `The movie ID exceeds the number of available shows. There are only ${allShows.length} shows.`
+            )
+        }
         const result = showByMovies.all(id)
         return result
-    } catch (error) {
-        console.error('Error finding Shows')
+    } catch (e) {
+        console.error('Error finding Shows', e.message)
         return false
     }
 }
