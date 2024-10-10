161f5ab2383134f372e5d2d291ba278f2e2a8bad
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-10-10 15:29:23 +0200

    Fixed some function

--- backend/src/service/bookingsService.js
+++ backend/src/service/bookingsService.js
@@ -1,4 +1,5 @@
 import { db } from '../../server.js'
+import mapBookings from '../utils/mapBookings.js'
 
 const getBookings = (email, role) => {
     let booking
@@ -17,44 +18,7 @@ const getBookings = (email, role) => {
         booking = db.prepare(query).all(email)
     }
 
-    const result = booking
-
-    const bookingsMap = {}
-
-    result.forEach((booking) => {
-        const bookingId = booking.bookingId
-        if (bookingsMap[bookingId]) {
-            bookingsMap[bookingId].seats.push({
-                seatRow: parseInt(booking.seatRow, 10),
-                seatNumber: parseInt(booking.seatNumber, 10),
-                ticketType: booking.ticketType,
-                ticketPrice: booking.ticketPrice,
-            })
-        } else {
-            bookingsMap[bookingId] = {
-                bookingId: bookingId,
-                bookingNumberId: booking.bookingNumberId,
-                userId: booking.userId,
-                userEmail: booking.userEmail,
-                userFirstname: booking.userFirstname,
-                userLastname: booking.userLastname,
-                showTime: booking.showTime,
-                cinemaName: booking.cinemaName,
-                movieTitle: booking.movieTitle,
-                movieUrl: booking.moviePosterUrl,
-                seats: [
-                    {
-                        seatRow: parseInt(booking.seatRow, 10),
-                        seatNumber: parseInt(booking.seatNumber, 10),
-                        ticketType: booking.ticketType,
-                        ticketPrice: booking.ticketPrice,
-                    },
-                ],
-            }
-        }
-    })
-
-    return Object.values(bookingsMap)
+    return mapBookings(booking)
 }
 
 const getAllTickets = () => {
@@ -72,27 +36,14 @@ const getAllTickets = () => {
 }
 
 const getBookingFs = (bookingId) => {
-    const getSpecificBooking = `SELECT 
-  *
-FROM 
-  bookings
-JOIN 
-  shows ON bookings.showId = shows.Id
-JOIN 
-  movies ON shows.movieId = movies.Id
-JOIN 
-  cinemas ON shows.cinemaId = cinemas.Id
-JOIN 
-  bookingXseatsXticket ON bookings.Id = bookingXseatsXticket.bookingID
-JOIN 
-  cinemaSeats ON bookingXseatsXticket.cinemaSeatsID = cinemaSeats.Id
-JOIN 
-  ticketTypes ON bookingXseatsXticket.ticketTypeId = ticketTypes.Id
-WHERE 
-  bookings.Id = ?`
+    const getSpecificBooking = `
+    SELECT *
+        FROM userBookings
+    WHERE 
+    bookingId = ?`
 
-    const statement = db.prepare(getSpecificBooking).get(bookingId)
-    return statement
+    const statement = db.prepare(getSpecificBooking).all(bookingId)
+    return mapBookings(statement)
 }
 
 export default { getBookingFs, getAllTickets, getBookings }
