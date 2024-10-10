2f47430caa73930b69e4ca1a087337c02506e523
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-10-10 08:54:21 +0200

    adding wrong handling

--- backend/src/service/showsService.js
+++ backend/src/service/showsService.js
@@ -1,14 +1,14 @@
 import { db } from '../../server.js'
 
 const getAllShows = () => {
-    try {
-        const allshows =
-            'SELECT cinemas.name AS cinemaName, cinemas.Id AS cinemaId, shows.Id AS showId, shows.time AS showTime, movies.*  FROM shows INNER JOIN movies ON movieId = movies.id INNER JOIN cinemas ON shows.cinemaId = cinemas.id ORDER BY cinemas.name '
-        const stmt = db.prepare(allshows).all()
-        return stmt
-    } catch (e) {
-        throw new Error('Failed to get shows')
+    const allshows =
+        'SELECT cinemas.name AS cinemaName, cinemas.Id AS cinemaId, shows.Id AS showId, shows.time AS showTime, movies.*  FROM shows INNER JOIN movies ON movieId = movies.id INNER JOIN cinemas ON shows.cinemaId = cinemas.id ORDER BY cinemas.name '
+    const stmt = db.prepare(allshows).all()
+
+    if (stmt.length === 0) {
+        throw new Error({ msg: 'No shows found' })
     }
+    return stmt
 }
 
 export default { getAllShows }
