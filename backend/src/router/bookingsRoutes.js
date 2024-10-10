9fdc53cdc94fcc1f3760d82e8fd0b292298ff746
Ehnwall <dennis.ehnwall@gmail.com>
2024-10-10 09:17:36 +0200

    removed wrong commit from bookinController/Routes & Service

--- backend/src/router/bookingsRoutes.js
+++ backend/src/router/bookingsRoutes.js
@@ -1,8 +1,1 @@
-import Express from 'express'
-import bookingsController from '../controller/bookingsController.js'
 
-const router = Express.Router()
-
-router.get('/api/ticketsById/:id', bookingsController.getTicketsById)
-
-export default router
--- backend/src/service/bookingsService.js
+++ backend/src/service/bookingsService.js
@@ -1,10 +1,1 @@
 import { db } from '../../server.js'
-
-const getTicketsById = (id) => {
-    const sql = `SELECT * FROM ticketTypes WHERE Id = ?` // Korrekt sträng för SQL
-    const stmt = db.prepare(sql).get(id) // Använder SQL-strängen korrekt
-    console.log(stmt)
-    return stmt
-}
-
-export default { getTicketsById }
