133187f34e9de16f91b301c56f82a93cbc53c3d9
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-10-10 10:31:06 +0200

    removed database and abbreviations

--- backend/src/service/bookingsService.js
+++ backend/src/service/bookingsService.js
@@ -1,32 +1,26 @@
 import { db } from '../../server.js'
 
 const getBookingFs = (bookingId) => {
-    const getSpecificBooking = `
-    SELECT 
-      *
-    FROM 
-      bookings b
-    JOIN 
-      shows s ON b.showId = s.Id
-    JOIN 
-      movies m ON s.movieId = m.Id
-    JOIN 
-      cinemas c ON s.cinemaId = c.Id
-    JOIN 
-      bookingXseatsXticket bs ON b.Id = bs.bookingID
-      JOIN cinemaSeats ON bs.cinemaSeatsID = cinemaSeats.Id
-    JOIN 
-      ticketTypes tt ON bs.ticketTypeId = tt.Id
-    WHERE 
-      b.Id = ?
-`
-
-    /*
-    I rename, for example, shows as s , tt for ticket tipes and bs for booking seats 
-    */
+    const getSpecificBooking = `SELECT 
+  *
+FROM 
+  bookings
+JOIN 
+  shows ON bookings.showId = shows.Id
+JOIN 
+  movies ON shows.movieId = movies.Id
+JOIN 
+  cinemas ON shows.cinemaId = cinemas.Id
+JOIN 
+  bookingXseatsXticket ON bookings.Id = bookingXseatsXticket.bookingID
+JOIN 
+  cinemaSeats ON bookingXseatsXticket.cinemaSeatsID = cinemaSeats.Id
+JOIN 
+  ticketTypes ON bookingXseatsXticket.ticketTypeId = ticketTypes.Id
+WHERE 
+  bookings.Id = ?`
 
     const statement = db.prepare(getSpecificBooking).get(bookingId)
-
     return statement
 }
 
