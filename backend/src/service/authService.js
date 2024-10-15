d7a7ac12645f73ec4ce7bb164b149cf5e2805ca8
AwE9800 <143619434+AwE9800@users.noreply.github.com>
2024-10-15 15:44:16 +0200

    quests can now create a account

--- backend/src/service/authService.js
+++ backend/src/service/authService.js
@@ -28,16 +28,23 @@ const exists = async ({ email, password }) => {
 const create = async ({ email, firstName, lastName, password, role = 'user' }) => {
     const checkIfUserExistsQuery = 'SELECT * FROM users WHERE email = ?'
     const insertUserQuery = 'INSERT INTO users (email, firstName, lastName, password, role) VALUES (?, ?, ?, ?, ?)'
+    const updateUserQuery = 'UPDATE users SET firstName = ?, lastName = ?, password = ?, role = ? WHERE email = ?'
 
     const stmt = db.prepare(checkIfUserExistsQuery).get(email.toLowerCase())
-    if (stmt) {
+    if (stmt && stmt.role !== 'guest') {
         throw new Error('User already exists')
     }
 
     const salt = await bcrypt.genSalt(12)
     const hash = await bcrypt.hash(password, salt)
-    const result = db.prepare(insertUserQuery).run(email.toLowerCase(), firstName, lastName, hash, role)
-    return result
+
+    if (stmt && stmt.role === 'guest') {
+        const result = db.prepare(updateUserQuery).run(firstName, lastName, hash, role, email)
+        return result
+    } else {
+        const result = db.prepare(insertUserQuery).run(email, firstName, lastName, hash, role)
+        return result
+    }
 }
 
 export default { create, exists }
