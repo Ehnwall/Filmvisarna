3c58b3dca9cd6df7872ff13fe01dd803eaebcb67
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-10-22 08:58:56 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
