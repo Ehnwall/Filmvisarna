944e0d52e4a429003345280f8448adc3a362c793
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-10-23 15:33:15 +0200

    I have tried to make a datepicker

--- src/utils/api/shows/renderShows.tsx
+++ src/utils/api/shows/renderShows.tsx
@@ -1,6 +1,7 @@
-import { Container, Card, Badge, Stack } from 'react-bootstrap'
-import { BsClock } from 'react-icons/bs'
+import { Container, Card, Badge, Stack, Row, Col } from 'react-bootstrap'
 import { useGetShows } from '../shows/useGetShows'
+import Dropdown from 'react-bootstrap/Dropdown'
+import { BsArrowDown, BsClock, BsCalendar } from 'react-icons/bs'
 
 const MoviesWithCinnema = () => {
     const { data: shows, isLoading, error } = useGetShows()
@@ -12,8 +13,55 @@ const MoviesWithCinnema = () => {
 
     const storaSalongenShows = shows?.filter((show) => show.cinemaName === 'Stora salongen').slice(0, 5)
 
+    const getCurrentWeekInterval = () => {
+        const today = new Date()
+        const dayOfWeek = today.getDay()
+        const dayDiff = (dayOfWeek + 6) % 7
+        const startOfThisWeek = new Date(today.setDate(today.getDate() - dayDiff))
+
+        const endOfThisWeek = new Date(startOfThisWeek)
+        endOfThisWeek.setDate(startOfThisWeek.getDate() + 6)
+
+        return { start: startOfThisWeek, end: endOfThisWeek }
+    }
+
+    const currentWeek = getCurrentWeekInterval()
+
     return (
         <>
+            <Container className="py-5" id="book">
+                <h4 className="py-2 ps-2 bg-primary text-dark">Välj ett datum för bio </h4>
+                <Dropdown className="py-2">
+                    <Dropdown.Toggle className="btn-filter" variant="primary" id="dropdown-basic">
+                        Vecka 7 <BsArrowDown />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Vecka 7</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Row className="g-2">
+                    {new Array(7).fill(null).map((_, index) => {
+                        const dayOfWeek = new Date(currentWeek.start)
+                        dayOfWeek.setDate(currentWeek.start.getDate() + index)
+                        const options = { weekday: 'long', day: 'numeric', month: 'numeric' }
+                        const formattedDate = dayOfWeek.toLocaleDateString('sv-SE', options)
+
+                        return (
+                            <Col key={index}>
+                                <Card className="border card-dates">
+                                    <Card.Body>
+                                        <Card.Title>{formattedDate}</Card.Title>
+                                        <Badge className="py-2 d-inline-flex align-items-center " bg="secondary">
+                                            <BsCalendar className="me-2" /> Bio time
+                                        </Badge>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </Container>
+
             <Container className="py-5">
                 <h2>Lilla Salongen</h2>
                 <div className="horizontal-scrollable">
