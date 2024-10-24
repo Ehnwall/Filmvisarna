6d7793c46d03cad2764017778179a3d892169a2d
AwE9800 <143619434+AwE9800@users.noreply.github.com>
2024-10-24 10:01:15 +0200

    Removed the test auth before merge

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -1,12 +1,10 @@
 // pages/routes
 import MemberPage from '../pages/memberPage/index'
 import StartPage from '../pages/startPage/StartPage'
-import SignIn from './api/auth/testAuth'
 
 export default [
     { path: '/', element: <StartPage />, menuLabel: 'Hem' },
     { path: '/medlem', element: <MemberPage />, menuLabel: 'Medlem' },
-    { path: '/sign-in', element: <SignIn />, menuLabel: 'Logga in' },
 ]
 
 //Paths
