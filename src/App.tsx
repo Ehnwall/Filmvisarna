c33166ed737742b21efa7e4ccbb01643e94e8675
BomanStatic <boman.staticsites@gmail.com>
2024-10-28 10:56:46 +0100

    added timeFormat utlil and tanstack devtools

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet, useLocation } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Footer from './componets/footer/footer'
 import { AuthProvider } from './context/authContext'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function App() {
     const queryClient = new QueryClient()
@@ -14,12 +15,13 @@ export default function App() {
         <>
             <QueryClientProvider client={queryClient}>
                 <AuthProvider>
-                <div>header</div>
+                    <div>header</div>
                     <main>
                         <Outlet />
                     </main>
-                <Footer />
+                    <Footer />
                 </AuthProvider>
+                <ReactQueryDevtools initialIsOpen={false} />
             </QueryClientProvider>
         </>
     )
