5348b21d0a5e8dd6737aa59eff0be9ce92bdc9ef
Ehnwall <dennis.ehnwall@gmail.com>
2024-11-07 16:18:36 +0100

    add so form only show when user has choosen a movie

--- src/componets/admin/add/AddShow.tsx
+++ src/componets/admin/add/AddShow.tsx
@@ -1,5 +1,5 @@
 import { useGetMovies } from '../../../utils/api/movies/useGetMovies'
-import { Dropdown, ButtonGroup, Button } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 import { useState } from 'react'
 import { MOVIE } from '../../../utils/types/types'
 
@@ -7,20 +7,53 @@ export default function AddShow() {
     const [movie, setMovie] = useState<MOVIE | null>(null)
 
     const { data: movies } = useGetMovies()
+    const handleMovieChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedMovie = movies?.find((movie) => movie.Id === parseInt(event.target.value))
+        setMovie(selectedMovie || null)
+    }
     return (
         <div>
-            <h2>hello from add-show</h2>
-            <Dropdown as={ButtonGroup}>
-                <Button variant="primary">{movie ? movie?.title : 'Välj en film'}</Button>
-                <Dropdown.Toggle split variant="primary" id="dropdown-split-basic" />
-                <Dropdown.Menu>
-                    {movies?.map((movie) => (
-                        <Dropdown.Item key={movie.Id} onClick={() => setMovie(movie)}>
-                            {movie.title}
-                        </Dropdown.Item>
-                    ))}
-                </Dropdown.Menu>
-            </Dropdown>
+            <h2>Lägg till en ny visning</h2>
+
+            <Form.Select onChange={handleMovieChange}>
+                <option>Välj en film</option>
+                {movies?.map((movie) => (
+                    <option key={movie.Id} value={movie.Id}>
+                        {movie.title}
+                    </option>
+                ))}
+            </Form.Select>
+            {movie && (
+                <Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label className="mt-3">Tid för visning</Form.Label>
+                        <Form.Control type="datetime-local" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Salong för visning</Form.Label>
+                        <Form.Select>
+                            <option value="1">Stora salongen</option>
+                            <option value="2">Lilla salongen</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Button type="submit">Lägg till visning</Button>
+                </Form>
+            )}
         </div>
     )
 }
+
+//when(movie.?title) is clicked, the form should be displayed
+
+//the form should contain the following fields:
+// - date (input type date)
+// - time (input type time)
+// - submit button
+// - cancel button
+// - when the form is submitted, the show should be created in the database
+// - when the cancel button is clicked, the form should be hidden
+// - the form should be hidden by default
+// - the form should be displayed when a movie is selected
+// - the form should be hidden when the movie is unselected
+// - the form should be hidden when the cancel button is clicked
+// - the form should be hidden when the submit button is clicked
